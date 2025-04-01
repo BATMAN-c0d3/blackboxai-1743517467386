502db6da676cf45f869303fb9759192244089b4a
BATMAN-c0d3 <hero.cn1@gmail.com>
2025-04-01 16:24:47 +0200

    Add AgriTechAdvisor/src/screens/CropDiagnosisScreen.tsx 1743517486556

---
+++ AgriTechAdvisor/src/screens/CropDiagnosisScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CropDiagnosisScreen() {
+  return (
+    <View>
+      <Text>Crop Diagnosis Screen</Text>
+    </View>
+  );
+}
